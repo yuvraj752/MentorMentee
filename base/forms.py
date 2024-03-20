8bca1b1eb533de6d42c358d648776168a4a3654a
Yuvraj Rana <yuvrajrana752@gmail.com>
2024-03-20 09:22:02 +0530

    added floatinf fields and forms layout

--- base/forms.py
+++ base/forms.py
@@ -28,10 +28,10 @@ class LoginForm(AuthenticationForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         field_divs = []
-        for field_name, field in self.fields.items(): 
+        for field_name in self.fields.keys(): 
             field_divs.append(Div(FloatingField(field_name), css_class='col-md-12')) 
         self.helper.layout = Layout(Div(*field_divs, css_class='row'), 
-                                    Submit('submit', 'Login', css_class='btn btn-primary w-100 mb-2'))
+                                Submit('submit', 'Login', css_class='btn btn-primary w-100 mb-2'))
 
 
 class SetPasswordForm(SetPasswordForm):
