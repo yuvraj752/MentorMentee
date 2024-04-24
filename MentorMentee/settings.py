af0598d77224b366937ca5d6ce9b608ae8faed67
Yuvraj Rana <yuvrajrana752@gmail.com>
2024-04-24 09:02:43 +0530

    customized admin panel complete

--- MentorMentee/settings.py
+++ MentorMentee/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'MentorMentee.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#PostgreSQL
+# PostgreSQL
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.postgresql",
